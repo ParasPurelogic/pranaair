be7c57ab78f8ecfa1dbbe8253c811bb16c98fbad
Abhishek <abhi.kk716@gmail.com>
2025-05-22 10:37:24 +0530

    abhi

--- src/app/[lang]/solutions-by-application/bank-financial-institutions/page.js
+++ src/app/[lang]/solutions-by-application/bank-financial-institutions/page.js
@@ -561,15 +561,5 @@ export default function BankFinancePage() {
       </div>
     </div>
 
-      {/* Bootstrap Scripts */ }
-  <div
-    dangerouslySetInnerHTML={{
-      __html: `
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        `,
-    }}
-  />
-    </div >
   )
 }
